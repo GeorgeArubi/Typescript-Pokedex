e8ae7f487b4cd2fbdce0482a42f74f87b96b1802
DOGHOUSETV <gea53899@marymount.edu>
2020-11-07 16:05:05 -0500

    commit

---
+++ basic.js
@@ -0,0 +1,9 @@
+"use strict";
+function myTest(args) {
+    if (args.name) {
+        return "Hello " + args.name;
+    }
+    return "Hello World";
+}
+myTest({ id: 1 });
+//# sourceMappingURL=basic.js.map
